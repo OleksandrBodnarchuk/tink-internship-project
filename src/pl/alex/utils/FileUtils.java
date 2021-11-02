9cd48aa707509ed443b8c4bea4a793a2d776c66a
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-11-02 10:05:12 +0100

    2. Helper method for printing lines + method to get the file name from user.

--- src/pl/alex/utils/FileUtils.java
+++ src/pl/alex/utils/FileUtils.java
@@ -1,9 +1,24 @@
 package pl.alex.utils;
 
 
+import java.util.Scanner;
 
 public class FileUtils {
+    private final Scanner SCANNER = new Scanner(System.in);
+    private String ORIGINAL_FILE_PATH;
 
     public void start() {
+
+    }
+
+    private String getFileName() {
+        printLine("Enter the file name: ");
+        ORIGINAL_FILE_PATH = SCANNER.nextLine();
+        return ORIGINAL_FILE_PATH;
+    }
+
+    private void printLine(String lineToPrint) {
+        System.out.println("\n_________________________________________________\n"
+                + lineToPrint);
     }
 }
